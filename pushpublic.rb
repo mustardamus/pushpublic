616c59f7ec79e127ab1faf4dbb9bc70aad8892fd
Sebastian Senf <contact@mustardamus.com>
2010-05-30 22:26:01 +0200

    dynamically load article content and stylesheets

--- pushpublic.rb
+++ pushpublic.rb
@@ -1,8 +1,10 @@
-get "/css/:style.css" do
+#handle main stylesheet
+get "/css/main.css" do
   content_type "text/css", :charset => "utf-8"
-  less params[:style].to_sym
+  less :main
 end
 
+#handle article erb content
 get "/articles/:name" do
   article_dir = "articles/#{params[:name]}"
   article_erb = "#{article_dir}/article.erb"
@@ -14,10 +16,18 @@ get "/articles/:name" do
   end
 end
 
+#handle article stylesheet
+get "/articles/:name/:style.css" do
+  content_type "text/css", :charset => "utf-8"
+  less File.read "articles/#{params[:name]}/#{params[:style]}.less"
+end
+
+#index
 get "/" do
   erb :index
 end
 
+#404 error
 error 404 do
   erb :fof
 end
