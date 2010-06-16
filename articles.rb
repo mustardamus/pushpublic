e0c43186d5b532e7290a0fe0b0d43d2827bd908b
Sebastian Senf <contact@mustardamus.com>
2010-06-16 02:41:29 +0200

    ready to publish first article

--- articles.rb
+++ articles.rb
@@ -1,4 +1,5 @@
 ARTICLES = [
-  { :title => "jQuery Terminal Fun", :url => "jquery_terminal_fun", :created => Time.parse("06/03/2010") },
+  #{ :title => "Auto-Refresh your Browser on file change - The Ruby Way", :url => "auto_refresh_browser_on_file_change_the_ruby_way", :created => Time.parse("06/06/2010") },
+  #{ :title => "jQuery Terminal Fun", :url => "jquery_terminal_fun", :created => Time.parse("06/03/2010") },
   { :title => "Create a Micro Chameleon Blog with Sinatra", :url => "create_a_micro_chameleon_blog_with_sinatra", :created => Time.parse("05/31/2010") }
 ]
