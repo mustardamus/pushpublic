9d7c9418ae8363632c1b01930a9a4f2ed23032ab
Sebastian Senf <contact@mustardamus.com>
2010-06-18 16:32:06 +0200

    producin baby

--- articles.rb
+++ articles.rb
@@ -1,6 +1,5 @@
 ARTICLES = [
-  #{ :title => "Auto-Refresh your Browser on file change - The Ruby Way", :url => "auto_refresh_browser_on_file_change_the_ruby_way", :created => Time.parse("06/06/2010") },
-  #{ :title => "jQuery Terminal Fun", :url => "jquery_terminal_fun", :created => Time.parse("06/03/2010") },
+  { :title => "Auto-Refresh your Browser on file change - The Ruby Way", :url => "auto_refresh_browser_on_file_change_the_ruby_way", :created => Time.parse("06/18/2010") },
   { :title => "Creating Subdomains with Apache2", :url => "creating_subdomains_with_apache2", :created => Time.parse("06/18/2010") },
   { :title => "Forwarding E-Mails with Postfix", :url => "forwarding_emails_with_postfix", :created => Time.parse("06/16/2010") },
   { :title => "Create a Micro Chameleon Blog with Sinatra", :url => "create_a_micro_chameleon_blog_with_sinatra", :created => Time.parse("05/31/2010") }
